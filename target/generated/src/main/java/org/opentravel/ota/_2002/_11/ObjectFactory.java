62e9448c81c75d58b527960fa0ded928c72f52c9
namalfernandolk <namalakit@gmail.com>
2016-08-05 06:20:10 +0530

    First Commit

---
+++ target/generated/src/main/java/org/opentravel/ota/_2002/_11/ObjectFactory.java
@@ -0,0 +1,112 @@
+
+package org.opentravel.ota._2002._11;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the org.opentravel.ota._2002._11 package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.opentravel.ota._2002._11
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRS.Errors }
+     * 
+     */
+    public SessionCloseRS.Errors createSessionCloseRSErrors() {
+        return new SessionCloseRS.Errors();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRS.Warnings }
+     * 
+     */
+    public SessionCloseRS.Warnings createSessionCloseRSWarnings() {
+        return new SessionCloseRS.Warnings();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRS }
+     * 
+     */
+    public SessionCloseRS createSessionCloseRS() {
+        return new SessionCloseRS();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRS.Errors.Error }
+     * 
+     */
+    public SessionCloseRS.Errors.Error createSessionCloseRSErrorsError() {
+        return new SessionCloseRS.Errors.Error();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRS.Success }
+     * 
+     */
+    public SessionCloseRS.Success createSessionCloseRSSuccess() {
+        return new SessionCloseRS.Success();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRS.Warnings.Warning }
+     * 
+     */
+    public SessionCloseRS.Warnings.Warning createSessionCloseRSWarningsWarning() {
+        return new SessionCloseRS.Warnings.Warning();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRS.Errors.Error.ErrorInfo }
+     * 
+     */
+    public SessionCloseRS.Errors.Error.ErrorInfo createSessionCloseRSErrorsErrorErrorInfo() {
+        return new SessionCloseRS.Errors.Error.ErrorInfo();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRQ.POS.Source }
+     * 
+     */
+    public SessionCloseRQ.POS.Source createSessionCloseRQPOSSource() {
+        return new SessionCloseRQ.POS.Source();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRQ }
+     * 
+     */
+    public SessionCloseRQ createSessionCloseRQ() {
+        return new SessionCloseRQ();
+    }
+
+    /**
+     * Create an instance of {@link SessionCloseRQ.POS }
+     * 
+     */
+    public SessionCloseRQ.POS createSessionCloseRQPOS() {
+        return new SessionCloseRQ.POS();
+    }
+
+}
